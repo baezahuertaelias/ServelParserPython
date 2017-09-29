5283b164cf60c7fab539586919e076e3f179c898
Elias <baezahuertaelias@gmail.com>
2017-09-29 00:57:25 -0300

    Recursividad a los archivos txt y genera un csv
    Falta agregar los campos de la tabla

--- demo.py
+++ demo.py
@@ -1,14 +1,20 @@
 import re
-origen = open("Alto Hospicio.txt",'r',encoding='utf8')
-destino = open("Alto Hospicio.csv", "w")
+import os
 
-for line in origen:
-	if not "ELECTORAL" in line:
-		if not "PROVINCIA" in line:
-			line = re.sub('  +',';',line)
-			if line.startswith(";"):
-				nada = ""
-			else:
-				destino.write(line)
+destino = open("la dichosa bd.csv", "w")
+directorio = os.listdir()
+
+
+for files in directorio:
+	if files.endswith('.txt'):
+		origen = open(files,'r',encoding='utf8')
+		for line in origen:
+			if not "ELECTORAL" in line:
+				if not "PROVINCIA" in line:
+					line = re.sub('  +',';',line)
+					if line.startswith(";"):
+						nada = ""
+					else:
+						destino.write(line)
 origen.close()
 destino.close()
