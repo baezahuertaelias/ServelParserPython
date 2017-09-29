0a00852098b3819b1051623abe1dbc4553cd3989
Elias <baezahuertaelias@gmail.com>
2017-09-29 01:03:35 -0300

    Agregada la cabecera para el csv

--- demo.py
+++ demo.py
@@ -4,6 +4,7 @@ import os
 destino = open("la dichosa bd.csv", "w")
 directorio = os.listdir()
 
+destino.write("NOMBRE;RUT;SEXO;DIRECCION;COMUNA;MESA\n")
 
 for files in directorio:
 	if files.endswith('.txt'):
