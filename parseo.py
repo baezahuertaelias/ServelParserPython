516248580dd3881c8ef5f3377d0ff419eb5e2948
Elias <baezahuertaelias@gmail.com>
2017-09-29 22:23:14 -0300

    Revision si existe carpeta servel con los txt

--- parseo.py
+++ parseo.py
@@ -11,43 +11,44 @@ bar = ShadyBar('Cargantres')
 #Creo el csv con los datos y acceso escritura
 destino = open("la dichosa bd.csv", "w")
 
-#Leo el directorio y se guarda en array
-directorio = os.listdir()
+#Reviso si el directorio archivos existe, si no aviso al usuario
+if os.path.isdir("servel/") is True:
+	#Leo el directorio y se guarda en array
+	directorio = os.listdir()
 
-#Escribo la cabecera del csv
-destino.write("NOMBRE;RUT;SEXO;DIRECCION;COMUNA;MESA\n")
+	#Escribo la cabecera del csv
+	destino.write("NOMBRE;RUT;SEXO;DIRECCION;COMUNA;MESA\n")
 
+	#Agrego contador para saber la cantidad de archivos txt a procesar
+	#Estoy seguro que se puede hacer de una manera mas optimizada
+	contador = 0
+	for files in directorio:
+		if files.endswith('.txt'):
+			contador +=1
 
-#Agrego contador para saber la cantidad de archivos txt a procesar
-#Estoy seguro que se puede hacer de una manera mas optimizada
+			#Declarando la barra y el largo del contador anterior
+			bar = ShadyBar('Prcocesando archivos', max=contador)
 
-contador = 0
-for files in directorio:
-	if files.endswith('.txt'):
-		contador +=1
-
-#Declarando la barra y el largo del contador anterior
-bar = ShadyBar('Prcocesando archivos', max=contador)
-
-#Recorriendo el array con los archivos
-for files in directorio:
-	#Filtro los archivos planos
-	if files.endswith('.txt'):
-		bar.next()
-		#Despues de filtrar, los abro con utf8 por las tildes y ecnes
-		origen = open(files,'r',encoding='utf8')
-		#Leyendo las filas y filtro las que estan malas
-		for line in origen:
-			if not "ELECTORAL" in line:
-				if not "PROVINCIA" in line:
-					#Uso RegEx para poder eliminar esos espacios grandes entre los textos
-					line = re.sub('  +',';',line)
-					#Cuando encuentra una fila mal formateada, no la agrego al csv 
-					if line.startswith(";"):
-						nada = ""
-					else:
-						#Agrego al csv
-						destino.write(line)
-		origen.close()
-
-bar.finish()
+			#Recorriendo el array con los archivos
+			for files in directorio:
+				#Filtro los archivos planos
+				if files.endswith('.txt'):
+					bar.next()
+					#Despues de filtrar, los abro con utf8 por las tildes y ecnes
+					origen = open(files,'r',encoding='utf8')
+					#Leyendo las filas y filtro las que estan malas
+					for line in origen:
+						if not "ELECTORAL" in line:
+							if not "PROVINCIA" in line:
+								#Uso RegEx para poder eliminar esos espacios grandes entre los textos
+								line = re.sub('  +',';',line)
+								#Cuando encuentra una fila mal formateada, no la agrego al csv
+								if line.startswith(";"):
+									nada = ""
+								else:
+									#Agrego al csv
+									destino.write(line)
+					origen.close()
+					bar.finish()
+else:
+	print("No existe la carpeta \"servel\"")
